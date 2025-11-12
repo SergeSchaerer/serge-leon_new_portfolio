90362904dfb365eafaa1ed2f3cb2a1326d25de61
TAASCSE0 <serge-leon.schaerer@swisscom.com>
2025-11-12 15:23:06 +0100

    Update base path in Vite config and adjust asset links in index.html for local deployment

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: mode === 'production' ? '/serge-leon-showcase/' : '/',
+  base: "./",
   plugins: [
     react(),
     mode === 'development' && componentTagger(),
